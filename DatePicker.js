6ad1cf2558d77385dc3afc1f1a7d120b047f6209
PineappleFarms <123400127+PineappleFarms@users.noreply.github.com>
2023-09-22 12:03:34 -0400

    Variables in constructor to hold passed arguments from parameters.

--- DatePicker.js
+++ DatePicker.js
@@ -10,11 +10,17 @@ datePicker.render(new Date("July 4, 1776"));
 
 class DatePicker {
     // Constructor for initializing the DatePicker
+    // Parameters "div1" and function(id,fixedDate) from example in problem #1 on readMe
+    // Documentation for reference of constructor method https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/constructor
     constructor(id, callback) {
+        this.id = id; // Store the provided id
+        this.callback = callback; // Store the provided callback function
+        this.currentDate = null; // Initialize currentDate to null
     }
 
     // Render method to display the calendar in the specified container
     render(selectedDate) {
+
     }
 
 }
