570c03362085475e01961cb74a31a19d86d866cb
PineappleFarms <123400127+PineappleFarms@users.noreply.github.com>
2023-09-22 11:31:24 -0400

    Added skeleton code for js and ccs files required for problem one. Along with comments and instructions.

--- DatePicker.js
+++ DatePicker.js
@@ -1,1 +1,20 @@
+/*
+This file implements a JavaScript class named DatePicker that can be used as in the following example:
 
+    var datePicker = new DatePicker("div1", function (id, fixedDate) {
+        console.log("DatePicker with id", id,
+            "selected date:", fixedDate.month + "/" + fixedDate.day + "/" + fixedDate.year);
+    });
+datePicker.render(new Date("July 4, 1776"));
+ */
+
+class DatePicker {
+    // Constructor for initializing the DatePicker
+    constructor(id, callback) {
+    }
+
+    // Render method to display the calendar in the specified container
+    render(selectedDate) {
+    }
+
+}
