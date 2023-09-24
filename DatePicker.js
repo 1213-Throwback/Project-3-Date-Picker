99f95bc0999101ffda122e34385d39fbe85a05d8
PineappleFarms <123400127+PineappleFarms@users.noreply.github.com>
2023-09-24 16:56:20 -0400

    Filling in code for DatePicker.js skeleton. Still need to complete code/logic to populate the days of the month.

--- DatePicker.js
+++ DatePicker.js
@@ -12,8 +12,69 @@ function DatePicker(id, fixedDate) {
     this.id = id; // Store the provided id
     this.fixedDate = fixedDate; // Store the provided fixedDate
     // Render method to display the calendar
-    this.render = function (initialDate) {
+    this.render = function (fixedDate) {
+        // Utility function to create elements with class
+        // Including text is optional
+        function createElement(tag, className, text) {
+            const element = document.createElement(tag);
+            element.classList.add(className);
+            // Text present, include
+            if (text) {
+                element.innerText = text;
+            }
+            return element;
+        }
 
+        // Create container element to hold the rest of the calendar elements
+        const container = document.getElementById(this.id);
+        // The add() method of the DOMTokenList interface adds the given tokens to
+        // the list, omitting any that are already present.
+        container.classList.add('calendar');
+
+        // Create elements from datepicker.css file
+        const header = createElement('div', 'header');
+        const prevButton = createElement('button', 'prev-button', '<<');
+        const nextButton = createElement('button', 'next-button', '>>');
+        const monthLabel = createElement('div', 'month-year');
+
+        // The appendChild() method of the Node interface adds a node to the
+        // end of the list of children of a specified parent node.
+        // Parent node: container
+        // Child & Parent node: header
+        // Child nodes: prevButton, MonthLabel, nextButton
+        header.appendChild(prevButton);
+        header.appendChild(monthLabel);
+        header.appendChild(nextButton);
+        container.appendChild(header);
+
+        // String array holding days of the week
+        const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+        // Element for dayNames
+        const dayNames = createElement('div', 'day-names');
+
+        // The forEach() method executes a provided function once for each array element.
+        // Pull day names from daysOfWeek string array
+        daysOfWeek.forEach(function (dayName) {
+            const day = createElement('div', 'day', dayName);
+            dayNames.appendChild(day);
+        });
+
+        // Add to day names to 'calendar' container
+        container.appendChild(dayNames);
+
+        // Create element for days of month
+        const daysContainer = createElement('div', 'days');
+
+        // Still need to add logic to generate days of the month here-----------------------
+
+
+
+
+        //----------------------------------------------------------------------------------
+
+        // Add days of month to container
+        container.appendChild(daysContainer);
     };
 }
 
