55c6a71a62b793199aec87e3be6ea235125d497f
Zadok-3 <aduazadok@gmail.com>
2023-09-24 23:01:01 -0400

    I added some code for part 2 but it is not yet complete.

--- TableTemplate.js
+++ TableTemplate.js
@@ -5,9 +5,16 @@ class TableTemplate {
         //Variable Declaration and Initialization
         const table = document.getElementById(id);
         const headerRow = table.rows[0];
+        let  columnIndex = -1;
 
         for(let i = 0;i < headerRow.cells.length; i++ ){
             //Examine header row and replace properties
+            let cell = headerRow.cells[i];
+            cell.textContent = this.replaceProperty(cell.textContent, dictionary);
+            if (cell.textContent === columnName) {
+                columnIndex = i;
+            }
+
         }
 
         for(let i = 1; i < table.rows.length; i++){
@@ -20,6 +27,60 @@ class TableTemplate {
         }
 
 
+    static replaceProperty(text, dictionary) {
+        return text.replace(/{{(.*?)}}/g, (match, spot) => {
+            if (dictionary[spot]) {
+                return dictionary[spot];
+            } else {
+                return match;
+            }
+        });
     }
 
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
