33fbce97722e936af2fb640b2a27b39fa44d35b0
DJDooley <112581503+DJDooley@users.noreply.github.com>
2023-09-25 13:54:49 -0400

    Template code structure

--- TableTemplate.js
+++ TableTemplate.js
@@ -1,88 +1,47 @@
+'use strict'
 //Class Declaration
 class TableTemplate {
 
-    static replaceProperty(text, dictionary) {
-        return text.replace(/{{(.*?)}}/g, (match, spot) => {
-            if (dictionary[spot]) {
-                return dictionary[spot];
-            } else {
-                return match;
-            }
-        });
-    }
-
-    static fillIn(id, dictionary, columnName ){
+    static fillIn(id, dictionary, columnName) {
 
         //Variable Declaration and Initialization
         const table = document.getElementById(id);
         const headerRow = table.rows[0];
-        let  columnIndex = -1;
+        let columnIndex = -1;
+        let processingindex = null;
 
-        for(let i = 0;i < headerRow.cells.length; i++ ){
-            //Examine header row and replace properties
-            let cell = headerRow.cells[i];
-            cell.textContent = this.replaceProperty(cell.textContent, dictionary);
-            if (cell.textContent === columnName) {
-                columnIndex = i;
+
+
+        for (let i = 1; i < table.rows.length; i++) {
+            const row = table.rows[i];
+
+            if (columnName === undefined) {
+                const templateProcessor = new templateProcessor(table.innerHTML);
+                table.innerHTML = templateProcessor.fillIn(dictionary);
+
+            }else {
+                let templateProcessor = new templateProcessor(headerRow.innerHTML);
+                headerRow.innerHTML = templateProcessor.fillIn(dictionary);
+                for (let i = 0; i < headerRow.cells.length; i++) {
+                    if (headerRow.cells[i].innerHTML === columnName) {
+                        processingindex = i;
+                        break;
+                    }
+                }
+                if(processingindex != null){
+                    for(let a = 1; a < table.rows.length; a++){
+                        const row = table.rows[a];
+                        for(let b = 0; b < row.cells.length; b++){
+                            if(b === processingindex){
+                                const cell = row.cells[a];
+                                templateProcessor = new templateProcessor(headerRow.innerHTML);
+                                cell.innetHTML = templateProcessor.fillIn(dictionary);
+                            }
+                        }
+                    }
+                }
             }
-
         }
-
-        for(let i = 1; i < table.rows.length; i++){
-            if(columnName == null ){
-                //Process entire table
-            }
-            else{//columnName is not matched,
-                //Return without replacing the text in the columns
-            }
-        }
-
-
-
-
-
-
+        table.style.visibility = "visible";
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
